a78dc86866e3bc23bf0c9497fa87efa0bbf53ce4
mengdejun <mengdj@outlook.com>
2019-04-17 17:04:29 +0800

    1.0.8

--- decibel/decibel/decibel.h
+++ decibel/decibel/decibel.h
@@ -7,16 +7,17 @@
 
 #ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#define STBI_NO_BMP
-#define STBI_NO_PSD
-#define STBI_NO_TGA
-#define STBI_NO_HDR
-#define STBI_NO_PIC
-#define STBI_NO_PNM
-#define STBI_MSC_SECURE_CRT
 #include "stb_image.h"
+#endif
+
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#endif
+
+#ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
 #endif
 
 #ifdef _DEBUG
